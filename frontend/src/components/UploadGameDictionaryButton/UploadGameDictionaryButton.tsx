8ea3a727205ea979798994b9f30bcb0a82d6484d
Johan Lindqvist <jlt.johanlindqvist@gmail.com>
2024-06-08 19:46:59 +0200

    feat: add support for csv files

--- frontend/src/components/UploadGameDictionaryButton/UploadGameDictionaryButton.tsx
+++ frontend/src/components/UploadGameDictionaryButton/UploadGameDictionaryButton.tsx
@@ -1,13 +1,21 @@
 import { ChangeEvent, useRef } from 'react';
+import { usePapaParse } from 'react-papaparse';
 
 import { useSetupContext } from '~/hooks/useSetupContext';
+import { EDictionaryTypes, IWords } from '~/types';
 
 import { schema } from './schema';
 import { IUploadGameDictionaryButtonProps } from './types';
 
+const TYPE_JSON = 'application/json';
+const TYPE_CSV = 'text/csv';
+
 export function UploadGameDictionaryButton({ onFileUpload }: IUploadGameDictionaryButtonProps) {
   const { dictionary } = useSetupContext();
+  const { readString } = usePapaParse();
+
   const fileNameRef = useRef('');
+  const fileTypeRef = useRef('');
 
   const isJsonValid = async (json: any): Promise<boolean> => {
     try {
@@ -22,7 +30,11 @@ export function UploadGameDictionaryButton({ onFileUpload }: IUploadGameDictiona
   const onReaderLoad = async (event: ProgressEvent<FileReader>) => {
     const result = event.target?.result;
 
-    if (result) {
+    if (!result) {
+      return;
+    }
+
+    if (fileTypeRef.current === TYPE_JSON) {
       const json = JSON.parse(result as string);
 
       const valid = await isJsonValid(json);
@@ -30,6 +42,83 @@ export function UploadGameDictionaryButton({ onFileUpload }: IUploadGameDictiona
       if (valid) {
         onFileUpload({ fileName: fileNameRef.current, words: json });
       }
+    }
+
+    if (fileTypeRef.current === TYPE_CSV) {
+      readString<string[]>(result as string, {
+        worker: true,
+        complete: (results) => {
+          const columns: EDictionaryTypes[] = [
+            EDictionaryTypes.Easy,
+            EDictionaryTypes.Medium,
+            EDictionaryTypes.Hard,
+            EDictionaryTypes.Extreme,
+          ];
+
+          const isColumnKey = (column: string): column is EDictionaryTypes =>
+            columns.includes(column as EDictionaryTypes);
+
+          const wordsMap: Record<EDictionaryTypes, { words: string[]; index: number }> = {
+            easy: {
+              words: [],
+              index: -1,
+            },
+            medium: {
+              words: [],
+              index: -1,
+            },
+            hard: {
+              words: [],
+              index: -1,
+            },
+            extreme: {
+              words: [],
+              index: -1,
+            },
+          };
+
+          const [firstRow, ...restRows] = results.data;
+
+          firstRow.forEach((column, colIndex) => {
+            const col = column.toLowerCase();
+
+            if (isColumnKey(col)) {
+              wordsMap[col].index = colIndex;
+            }
+          });
+
+          const isInvalid = Object.values(wordsMap).some(({ index }) => index === -1);
+
+          if (isInvalid) {
+            // eslint-disable-next-line no-console
+            console.error('Invalid file format');
+            return;
+          }
+
+          restRows.reduce((acc, row) => {
+            columns.forEach((column) => {
+              const newWord = row[acc[column].index];
+
+              acc[column].words = [...acc[column].words, newWord];
+            });
+
+            return acc;
+          }, wordsMap);
+
+          const words = Object.entries(wordsMap).reduce<IWords>(
+            (acc, [column, { words: columnWords }]) => {
+              if (isColumnKey(column)) {
+                acc[column] = columnWords;
+              }
+
+              return acc;
+            },
+            { easy: [], medium: [], hard: [], extreme: [] },
+          );
+
+          onFileUpload({ fileName: fileNameRef.current, words });
+        },
+      });
     }
   };
 
@@ -41,6 +130,7 @@ export function UploadGameDictionaryButton({ onFileUpload }: IUploadGameDictiona
       const reader = new FileReader();
 
       fileNameRef.current = file.name;
+      fileTypeRef.current = file.type;
 
       reader.onload = onReaderLoad;
       reader.readAsText(file);
@@ -50,7 +140,7 @@ export function UploadGameDictionaryButton({ onFileUpload }: IUploadGameDictiona
   return (
     <div>
       <label htmlFor="upload">{dictionary ? `Cached file: ${dictionary.fileName}` : 'No file'}</label>
-      <input type="file" name="upload" accept="application/json" onChange={onFileChange} />
+      <input type="file" name="upload" accept="text/csv,application/json" onChange={onFileChange} />
     </div>
   );
 }
